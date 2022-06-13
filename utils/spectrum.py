24360dd6f09b31f742894c095d4e5c548aedd0cd
gamem0de <maddoc988@gmail.com>
2022-06-13 09:03:53 +0200

    utils: refactored spectrum plot to dB
    Signed-off-by: gamem0de <maddoc988@gmail.com>

--- utils/spectrum.py
+++ utils/spectrum.py
@@ -4,6 +4,43 @@ import scipy.fftpack as fftpk
 import numpy as np
 from matplotlib import pyplot as plt
 
+def dbfft(x, fs, win=None, ref=32768):
+    """
+    Calculate spectrum in dB scale
+    Args:
+        x: input signal
+        fs: sampling frequency
+        win: vector containing window samples (same length as x).
+             If not provided, then rectangular window is used by default.
+        ref: reference value used for dBFS scale. 32768 for int16 and 1 for float
+    Returns:
+        freq: frequency vector
+        s_db: spectrum in dB scale
+    """
+    N = len(x)  # Length of input sequence
+    if win is None:
+        win = np.ones(N)
+    if len(x) != len(win):
+            raise ValueError('Signal and window must be of the same length')
+    x = x * win
+    # Calculate real FFT and frequency vector
+    sp = np.fft.rfft(x)
+    freq = np.arange((N / 2) + 1) / (float(N) / fs)
+
+    # Scale the magnitude of FFT by window and factor of 2,
+    # because we are using half of FFT spectrum.
+    s_mag = np.abs(sp) * 2 / np.sum(win)
+
+    # Convert to dBFS
+    s_dbfs = 20 * np.log10(s_mag/ref)
+    
+    if len(freq) > len(s_dbfs):
+        freq = freq[:len(s_dbfs)]
+    if len(s_dbfs) > len(freq):
+        s_dbfs = s_dbfs[:len(freq)]
+        
+    return freq, s_dbfs
+
 def show_spectrum(data: str):
     s_rate, signal = wavfile.read(data) 
     print (f"Czestotliwosc probkowania: {s_rate} Hz")
@@ -18,11 +55,11 @@ def show_spectrum(data: str):
     Ts = 1.0/s_rate # sampling interval in time
     print (f"Okres T pomiedzy kolejnymi probkami: {Ts}s")
 
-    FFT = abs(scipy.fft.fft(signal))
-    freqs = fftpk.fftfreq(len(FFT), (1.0/s_rate))
+    freqs, FFT = dbfft(signal, s_rate)
 
     plt.xscale("log")
-    plt.plot(freqs[range(100, len(FFT)//2)], FFT[range(100, len(FFT)//2)])                                                          
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel('Amplitude')
+    plt.plot(freqs, FFT)                                                          
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Amplitude [dB]')
+    plt.grid(True)
     plt.show()
