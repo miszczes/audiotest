3a58b0e6a133f3d2a6ad665ee66a32f7d74f7f18
gamem0de <maddoc988@gmail.com>
2022-06-20 21:24:49 +0200

    devices: add output device selection dropdown menu
    Signed-off-by: gamem0de <maddoc988@gmail.com>

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ import sounddevice as sd
 from tkinter import *
 from PIL import ImageTk, Image
 
+def filter_devices(in_out: str):
+    devices = []
+    inout_devices =[]
+    for dict in sd.query_devices():
+        for key, value in dict.items():
+            if (key == in_out) and value > 0:
+                    devices.append(dict)
+    for dictio in devices:
+        for key, value in dictio.items():
+            if key == "name":
+                inout_devices.append(value)
+    return inout_devices
+
 def change_textlabel(mystring, text: str):
     mystring.set(text)
     root_tk.update()
@@ -34,8 +47,8 @@ def recording(length: str, device: str):
     cmd_rec = ["python", "utils/mic_rec.py", "-D", length, "-d", device]
     subprocess.run(cmd_rec)
 
-def playback(filename: str):
-    cmd_play = ["python", "utils/playback.py", filename]
+def playback(filename: str, device: str):
+    cmd_play = ["python", "utils/playback.py", filename, "-d", device]
     subprocess.run(cmd_play)
 
 def init_test(timeout: int, filename: str):
@@ -47,8 +60,8 @@ def init_test(timeout: int, filename: str):
         time.sleep(1)
     print("Standby...")
     change_textlabel(my_string, "Badanie rozpoczete, prosze zachowac cisze!")
-    thread1 = threading.Thread(recording(str(return_length(filename)), variable.get()))
-    thread2 = threading.Thread(playback(filename))
+    thread1 = threading.Thread(recording(str(return_length(filename)), in_variable.get()))
+    thread2 = threading.Thread(playback(filename, out_variable.get()))
 
     thread1.start()
     thread2.start()
@@ -56,11 +69,10 @@ def init_test(timeout: int, filename: str):
     thread1.join()
     thread2.join()
 
-    recnames = os.listdir("recordings/")
-    get_filevalues("recordings/"+recnames[-1])
 def click_action(button):
     button.pack_forget() #przycisk znika
-    drop.pack_forget() #menu rozwijane znika
+    in_drop.pack_forget() #menu rozwijane znika
+    out_drop.pack_forget()
     root_tk.update()
     init_test(5, "chirp.wav") #rozpoczyna sie badanie
     plotnames = glob.glob(os.getcwd() + '/plots/*.png')
@@ -78,12 +90,15 @@ def click_action(button):
         frame.place(anchor='center', relx=0.5, rely=0.5)
         img = ImageTk.PhotoImage(Image.open(latest_file))
         label = Label(frame, image = img)
-        drop.pack()
+        in_drop.pack()
         button.pack()
+        out_drop.pack()
         label.pack()
         button.config(text=f"Wykonaj Badanie Ponownie!")
         root_tk.update()
         my_string.set(f"Badanie zakonczone! Wynik znajduje sie w\n{latest_file}")
+        recnames = os.listdir("recordings/")
+        get_filevalues("recordings/"+recnames[-1])
     Tk.update(self=None)
 
 def create_command(func, *args, **kwargs):
@@ -91,7 +106,6 @@ def create_command(func, *args, **kwargs):
         return func(*args, **kwargs)
     return command
 
-devices = []
 root_tk = Tk()
 root_tk.geometry("250x100")
 root_tk.title("AudioTest")
@@ -100,17 +114,18 @@ my_string = StringVar()
 footer_text = StringVar()
 label = Label(root_tk, textvariable=my_string)
 footer = Label(root_tk, textvariable=footer_text)
-variable = StringVar(root_tk)
-variable.set(sd.query_devices(kind = 'input')["name"]) # default value
-for dict in sd.query_devices():
-    for key, value in dict.items():
-        if key == 'name':
-            devices.append(value)
-
-drop = OptionMenu( root_tk , variable, *devices)
-drop.place(anchor='nw')
-drop.pack()
+in_variable = StringVar(root_tk)
+in_variable.set(sd.query_devices(kind = 'input')["name"]) # default value
+out_variable = StringVar(root_tk)
+out_variable.set(sd.query_devices(kind = 'output')["name"]) # default value
+in_devices = filter_devices("max_input_channels")
+out_devices = filter_devices("max_output_channels")
+in_drop = OptionMenu( root_tk , in_variable, *in_devices)
+out_drop = OptionMenu( root_tk , out_variable, *out_devices)
+in_drop.place(anchor='nw')
+in_drop.pack()
 click_button.pack()
+out_drop.pack()
 label.pack()
 footer.pack()
 click_button.config(command=create_command(click_action, click_button))
